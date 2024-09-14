cc7d0c3da38f2f7797d9105e2cc36f291da8417a
AlhoussinAmin <Alhoussin@34ml.com>
2024-09-14 22:16:29 +0300

    add image example

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="h-screen w-screen flex justify-center items-center">
+    <div class="h-1/2 w-1/2">
+      <ZoomImg :src="red" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts">
+import ZoomImg from "./components/ZoomImg.vue";
+import red from "./assets/images/red.jpg";
+import "./assets/css/main.css";
+</script>
