f616698bf1ce00698952616cce578e15dbc271c2
HossinAmin <hossinamin70@gmail.com>
2024-10-12 14:30:50 +0300

    reset me

--- src/components/Wrapper/ZoomMapWrapper.vue
+++ src/components/Wrapper/ZoomMapWrapper.vue
@@ -10,7 +10,7 @@
     <ZoomMap
       class="absolute -top-[26%] left-[1%] h-[25%] w-[25%] !border-2 !border-solid !border-white"
       :src="src"
-      :zoom-scale="zoomScale"
+      :zoom-scale="scale"
       :position="windowPosition"
       @update:position="updateOffset"
     />
@@ -40,14 +40,17 @@ const scale = ref(1);
 const windowPosition = computed(() => {
   if (scale.value !== 1) {
     return {
-      left: -zoomedImgOffset.value.left / (props.zoomScale * 4),
-      top: -zoomedImgOffset.value.top / (props.zoomScale * 4),
+      left: -zoomedImgOffset.value.left / (scale.value * 4),
+      top: -zoomedImgOffset.value.top / (scale.value * 4),
     };
   }
 });
 
 const updateOffset = (newPosition?: PositionType) => {
-  scale.value = 2;
+  if (scale.value === 1) {
+    scale.value = props.zoomScale;
+  }
+
   zoomedImgOffset.value = {
     left: -Number(newPosition?.left ?? 0) * (props.zoomScale * 4),
     top: -Number(newPosition?.top ?? 0) * (props.zoomScale * 4),
