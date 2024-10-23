9efc2da3a2cf3e98f74a8d7f8520d3a2db622e6d
HossinAmin <hossinamin70@gmail.com>
2024-10-23 22:51:57 +0300

    fix cursor styles

--- src/composables/useMultiZoom.ts
+++ src/composables/useMultiZoom.ts
@@ -6,16 +6,16 @@ export default function useMultiZoom() {
   const zoomInOut = (scale: number, maxScale: number, step: number) => {
     let zoomScale = scale;
 
-    if (zoomScale === 1) {
-      zoomDir.value = "IN";
-    } else if (zoomScale === maxScale) {
-      zoomDir.value = "OUT";
-    }
-
     if (zoomDir.value === "IN") {
       zoomScale = Math.min(scale + step, maxScale);
     } else {
       zoomScale = Math.max(scale - step, 1);
+    }
+
+    if (zoomScale === 1) {
+      zoomDir.value = "IN";
+    } else if (zoomScale === maxScale) {
+      zoomDir.value = "OUT";
     }
 
     return zoomScale;
