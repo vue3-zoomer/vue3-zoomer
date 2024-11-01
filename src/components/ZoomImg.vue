888541be3a91d415c9d12d3e3c3328284a6f39cb
MohamedAmr21 <ma4078458@gmail.com>
2024-11-01 23:00:29 +0200

    add the buttons to the zoomMap

--- src/components/ZoomImg.vue
+++ src/components/ZoomImg.vue
@@ -1,12 +1,37 @@
 <template>
-  <MoveZoomImg v-if="zoomType === 'move'" v-bind="props" />
-  <DragZoomImg v-else v-bind="props" />
+  <div class="w-1/2">
+    <MoveZoomImg
+      v-if="zoomType === 'move'"
+      v-model:current-scale="currentScale"
+      v-bind="props"
+      ref="exposeMove"
+      :persist="true"
+      @zoomIn="handleZoomIn"
+      @zoomOut="handleZoomOut"
+    />
+    <DragZoomImg
+      v-else
+      v-bind="props"
+      v-model:current-scale="currentScale"
+      ref="exposeMove"
+      @zoomIn="handleZoomIn"
+      @zoomOut="handleZoomOut"
+    />
+    <AddSubBtn
+      v-if="showAddSubBtn"
+      :max-zoom="currentScale === zoomScale"
+      :min-zoom="currentScale === 1"
+      @zoomIn="handleZoomIn"
+      @zoomOut="handleZoomOut"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-import type { PropType } from "vue";
+import { PropType, ref, useTemplateRef } from "vue";
 import DragZoomImg from "~/components/core/DragZoomImg.vue";
 import MoveZoomImg from "~/components/core/MoveZoomImg.vue";
+import AddSubBtn from "./controls/AddSubBtn.vue";
 
 const props = defineProps({
   src: {
@@ -31,5 +56,26 @@ const props = defineProps({
   persist: {
     type: Boolean,
   },
+  showAddSubBtn: {
+    type: Boolean,
+    default: true,
+  },
 });
+
+const exposeMove = useTemplateRef("exposeMove");
+const currentScale = ref(1);
+
+const handleZoomIn = () => {
+  if (exposeMove.value) {
+    exposeMove.value.zoomDir = "IN";
+    exposeMove.value.multiZoom();
+  }
+};
+
+const handleZoomOut = () => {
+  if (exposeMove.value) {
+    exposeMove.value.zoomDir = "OUT";
+    exposeMove.value.multiZoom();
+  }
+};
 </script>
