283cf2724c165529280381f086f752a50be1149e
HossinAmin <hossinamin70@gmail.com>
2024-11-02 12:56:49 +0200

    css configs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,36 +1,37 @@
-import { defineConfig } from 'vitepress'
-import path, { resolve } from 'path'
+import { defineConfig } from "vitepress";
+import path, { resolve } from "path";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Vue Zoom Components",
+  title: "Vue Zoom",
   description: "A VitePress Site",
   vite: {
     resolve: {
       alias: {
-        "~": path.join(process.cwd(),"/src"),
+        "~": path.join(process.cwd(), "/src"),
       },
     },
   },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide" },
+      { text: "Examples", link: "/guide" },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: "Examples",
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
