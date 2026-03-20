c1cc1496be267d2f86dc9e22e62e5c5ae17823e2
ahmedd-mahmoud <ahmed.mahran831@gmail.com>
2026-03-20 13:06:55 +0200

    chore: add test cases

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="centered-container">
-    <ZoomImg
+    <!-- <ZoomImg
       class="h-[30rem]"
       :src="red"
       :zoom-scale="5"
-      zoom-type="drag"
+      zoom-type="move"
       :step="1"
-      :show-zoom-btns="false"
+      trigger="hover"
+      :show-zoom-btns="true"
+      fullscreen
     />
+
+    <Magnifier class="h-[30rem]" :src="red" :zoom-scale="5" :size="200" /> -->
+
+    <FullscreenSlotExample />
   </div>
 </template>
 
@@ -15,15 +21,18 @@
 import red from "./assets/images/red.jpg";
 import "./assets/css/main.css";
 import ZoomImg from "./components/ZoomImg.vue";
+import Magnifier from "./components/Magnifier.vue";
+import FullscreenSlotExample from "./components/FullscreenSlotExample.vue";
 </script>
 
 <style scoped>
 .centered-container {
   display: flex;
-  height: 100vh;
-  width: 100vw;
+  /* height: 100vh; */
+  /* width: 100vw; */
   align-items: center;
   justify-content: center;
   padding: 1.25rem;
+  gap: 2rem;
 }
 </style>
